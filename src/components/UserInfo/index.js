b91660c172d3e481d4afe7927d2510ba48830544
Prasad8020 <varaprasad8020@gmail.com>
2023-08-27 14:08:10 +0000

    solution

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,12 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img src="" className="profile-img" alt="profile" />
+    <h1 className="user-name">Wade Warren</h1>
+    <p className="user-designation">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
